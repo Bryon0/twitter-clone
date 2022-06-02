edffe62a9273e6781721634d68d329cbaf0cdd8e
Bryon0 <bryon.c005@gmail.com>
2022-06-01 20:01:50 -0500

    Added the ability to store tweets

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import { useSession } from 'next-auth/react'
+import NewTweet from 'components/NewTweet'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+  return (
+    <div>
+      {session ? <NewTweet /> : <p>You are not logged in 😞</p>}
+    </div>
+  )
+}
